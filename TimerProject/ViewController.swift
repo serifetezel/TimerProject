741edf3031ccdadb808bf1ff7224f8ad324b67f2
Şerife Tezel <serifetezel15@gmail.com>
2023-09-10 00:43:00 +0300

    Initial commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by MacbookPro on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction),userInfo: nil, repeats: true)
+                
+    }
+    
+   @objc func timerFunction()
+    {
+        timeLabel.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time's Over"
+        }
+        
+    }
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("Button Clicked")
+    }
+}
+
